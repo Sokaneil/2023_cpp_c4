b4c8ccb059134868f5e807aa7145518f0d9dbee9
sokaneil.sieng <sokaneil.sieng@ecole-89.com>
2023-10-04 09:26:01 +0200

    chapter 4

--- ami/main.cpp
+++ ami/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "AsciiMap.hpp"
 #include "AsciiMapClip.hpp"
+#include "IAsciiMap.hpp"
 
 void mk_line(IAsciiMap &map, unsigned int y)
 {
@@ -16,6 +17,28 @@ void mk_line(IAsciiMap &map, unsigned int y)
     }
 }
 
+void mk_col(IAsciiMap &map, unsigned int x)
+{
+    auto i{0U};
+
+    while (i < map.height()) {
+        if (map.get_cell(x, i) == '-') {
+            map.set_cell(x, i, '+');
+        } else {
+            map.set_cell(x, i, '|');
+        }
+        i += 1;
+    }
+}
+
+void draw_o(IAsciiMap &clip)
+{
+    clip.set_cell(1, 0, '-');
+    clip.set_cell(0, 1, '|');
+    clip.set_cell(2, 1, '|');
+    clip.set_cell(1, 2, '-');
+}
+
 void draw_x(IAsciiMap &clip)
 {
     clip.set_cell(0, 0, '\\');
@@ -28,15 +51,25 @@ void draw_x(IAsciiMap &clip)
 int main(void)
 {
     try {
-        AsciiMap     map{5, 5};
-        AsciiMapClip clip(map);
+        AsciiMap     map{11, 11};
+        AsciiMapClip TopLeft(map), TopRight(map), TopCenter(map);
 
-        mk_line(map, 2);
-        draw_x(clip);
+        TopCenter.set_origin(4, 0);
+        TopCenter.set_size(3, 3);
+        TopLeft.set_origin(0, 0);
+        TopLeft.set_size(3, 3);
+        TopRight.set_origin(8, 0);
+        TopRight.set_size(3, 3);
+
+        mk_line(map, 3);
+        mk_line(map, 7);
+        mk_col(map, 3);
+        mk_col(map, 7);
+        draw_o(TopRight);
+        draw_o(TopCenter);
+        draw_x(TopLeft);
         map.to_stream(std::cout);
     } catch (std::out_of_range &err) {
-        std::cerr << "Error: " << err.what() << std::endl;
+        std::cerr << "Error:" << err.what() << std::endl;
     }
-
-    return 0;
 }
